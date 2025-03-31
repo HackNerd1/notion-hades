74090ba3971466b4af243fb5d7ec40a1ff142019
Hansel <heshenghong.hsh@gongdao.com>
2025-03-31 18:01:44 +0800

    fix: 修复循环调用问题

--- src/components/notion/notionToggle.tsx
+++ src/components/notion/notionToggle.tsx
@@ -2,7 +2,7 @@ import { BlockModel } from "@/models/notion.model";
 import NotionRichText from "./notionRichText";
 import { notionApiGetBlocks } from "@/apis/notion-apis";
 import { classNames } from "@/utils/main.utils";
-import { NotionBlock } from "./notionBlock";
+// import { NotionBlock } from "./notionBlock";
 
 interface NotionToggleProps {
   block: BlockModel;
@@ -22,7 +22,8 @@ export async function NotionToggle({ block, index = 0 }: NotionToggleProps) {
     <details className={detailsClass}>
       <summary className="cursor-pointer">
         <span className="ml-1">
-          <NotionBlock {...block} />
+          {/* <NotionBlock {...block} /> */}
+          <NotionRichText richText={block.richText} />
         </span>
       </summary>
       {blocks && blocks.map((block) => <NotionToggle key={block.id} block={block} index={index + 1}></NotionToggle>)}
