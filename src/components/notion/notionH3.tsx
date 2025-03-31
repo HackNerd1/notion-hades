24ce0b2061d8212aaab87a2f398cfad79dca1c90
Hansel <heshenghong.hsh@gongdao.com>
2025-03-31 18:03:50 +0800

    feat: 更新h1 h2 h3 p

--- src/components/notion/notionH3.tsx
+++ src/components/notion/notionH3.tsx
@@ -1,11 +1,10 @@
 import { BlockModel } from "@/models/notion.model";
-import { NotionToggle } from "./notionToggle";
 import NotionRichText from "./notionRichText";
 
 export function NotionH3(props: BlockModel) {
   return (
     <h3 id={props.id} className="mb-2 mt-4 text-lg font-bold md:text-xl">
-      {props.isToggleable ? <NotionToggle block={props} /> : <NotionRichText richText={props.richText} isTitle />}
+      <NotionRichText richText={props.richText} isTitle />
     </h3>
   );
 }
