3a9d5e85916a4f397ea3151d57119530765e8088
Hansel <heshenghong.hsh@gongdao.com>
2025-04-01 17:59:13 +0800

    feat: 开启图片优化 允许加载https图片

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    unoptimized: true
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // 允许任何 HTTPS 图片
+      },
+    ],
   },
 };
 
