2de958bcd51bfcfc3c9080887acc37cbf4138845
Hansel <heshenghong.hsh@gongdao.com>
2025-04-21 17:31:13 +0800

    feat: 更新sitemap动态生成功能

---
+++ next-sitemap.config.js
@@ -0,0 +1,47 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL,
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 7000,
+  exclude: ['/api/*'],
+  robotsTxtOptions: {
+    additionalSitemaps: [],
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+  additionalPaths: async (config) => {
+    // 获取所有文章
+    const results = []
+    let hasMore = true
+    let nextCursor = undefined
+    while (hasMore) {
+      const response = await fetch(`${process.env.SITE_URL}/api/posts`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nextCursor })
+      })
+
+      const posts = await response.json()
+      results.push(...posts.results)
+      hasMore = posts.has_more
+      nextCursor = posts.next_cursor
+    }
+
+    // 为每篇文章生成路径
+    const paths = results.map((post) => ({
+      loc: `/post/${post.id}`,
+      changefreq: 'daily',
+      priority: 0.8,
+      lastmod: new Date(post.last_edited_time).toISOString(),
+    }));
+
+    return paths;
+  },
+} 
